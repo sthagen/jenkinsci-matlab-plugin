143150b5d3d16d9b79c3e171850ae0cc559c6f47
Kapil Gupta <111856426+mw-kapilg@users.noreply.github.com>
2026-03-12 11:42:45 +0530

    Prevent TestResultsSummaryPlugin from throwing jsonencode related errors (#43)
    * add try block to prevent plugin from throwing jsonencode errors
    
    * update as per feedback

--- src/main/resources/+ciplugins/+jenkins/TestResultsViewPlugin.m
+++ src/main/resources/+ciplugins/+jenkins/TestResultsViewPlugin.m
@@ -1,5 +1,5 @@
 classdef TestResultsViewPlugin < matlab.unittest.plugins.TestRunnerPlugin
-    % Copyright 2025 The MathWorks, Inc.
+    % Copyright 2025-26 The MathWorks, Inc.
     
     methods (Access=protected)
         function reportFinalizedSuite(plugin, pluginData)
@@ -20,14 +20,19 @@ classdef TestResultsViewPlugin < matlab.unittest.plugins.TestRunnerPlugin
                 testResults = {};
             end
             testResults{end+1} = testDetails;
-            JsonTestResults = jsonencode(testResults, "PrettyPrint", true);
 
-            [fID, msg] = fopen(testArtifactFile, "w");
-            if fID == -1
-                warning("ciplugins:jenkins:TestResultsViewPlugin:UnableToOpenFile","Could not open a file for Jenkins tests result table due to: %s", msg);
-            else
-                closeFile = onCleanup(@()fclose(fID));
-                fprintf(fID, '%s', JsonTestResults);
+            try
+                jsonTestResults = jsonencode(testResults, "PrettyPrint", true);
+
+                [fID, msg] = fopen(testArtifactFile, "w");
+                if fID == -1
+                    warning("ciplugins:jenkins:TestResultsViewPlugin:UnableToOpenFile","Unable to open a file required to create the table of test results. (Cause: %s)", msg);
+                else
+                    closeFile = onCleanup(@()fclose(fID));
+                    fprintf(fID, '%s', jsonTestResults);
+                end
+            catch e
+                warning("ciplugins:jenkins:TestResultsViewPlugin:UnableToJsonEncode","Unable to jsonencode test results data. (Cause: %s)", e.message);
             end
 
             % Invoke the superclass method
